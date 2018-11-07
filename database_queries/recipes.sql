18b80ef8232be149cb7e013f1174d90fa0be9a53
sofia.bravo <sbr@hellofresh.com>
2018-11-07 13:42:27 +0100

    Added locale

--- database_queries/recipes.sql
+++ database_queries/recipes.sql
@@ -1,9 +1,9 @@
 SELECT title, difficulty, calories, prep_time
 FROM dimensions.recipe_dimension
-WHERE difficulty = '1' AND calories < 500
+WHERE difficulty = '1' AND calories < 500 AND locale = 'US'
 
 UNION
 
 SELECT title, difficulty, calories, prep_time
 FROM dimensions.recipe_dimension
-WHERE difficulty in ('1', '2') AND prep_time = 'PT20M'
+WHERE difficulty in ('1', '2') AND prep_time = 'PT20M' AND locale = 'US'
