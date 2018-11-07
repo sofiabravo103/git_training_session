5f33441a5b88e5fcc44462e805e996ebb0cab36e
sofia.bravo <sbr@hellofresh.com>
2018-11-07 13:24:33 +0100

    fixed main

--- database_queries/recipes.sql
+++ database_queries/recipes.sql
@@ -1,9 +1,9 @@
 SELECT title, difficulty, calories, prep_time
 FROM dimensions.recipe_dimension
-WHERE difficulty = '1' AND calories < 500 AND locale = 'US'
+WHERE difficulty = '1' AND calories < 500
 
 UNION
 
 SELECT title, difficulty, calories, prep_time
 FROM dimensions.recipe_dimension
-WHERE difficulty in ('1', '2') AND prep_time = 'PT20M' AND locale = 'US'
+WHERE difficulty in ('1', '2') AND prep_time = 'PT20M'
